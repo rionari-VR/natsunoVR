17860f3e3b08093eb130614a207d589a3fcade97
ktsuji <aoriochiki@gmail.com>
2020-08-04 19:27:44 +0900

    Add:: bullet prefab & GunController & GunObject

---
+++ Assets/RW/Scripts/GunController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private float bulletSpeed;
+
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            MakeBullet();
+        }
+    }
+
+    void MakeBullet()
+    {
+        GameObject bulletObj = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Rigidbody bulletRb = bulletObj.GetComponent<Rigidbody>();
+        bulletRb.AddForce(transform.forward * bulletSpeed);
+        Destroy(bulletObj, 3.0f);
+    }
+}
