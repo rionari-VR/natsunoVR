1769fe5264d147d6869ca4ba102e5cf9a5ecff64
TaruK981221 <tarui2412git@gmail.com>
2020-08-09 14:58:13 +0900

    アセット「ProBuilder」追加 制作用シーン「MakeGimick」追加 回転するステージ作成 ステージにオブジェクトが出現するようにした

---
+++ Assets/Scripts/Tarui'sScript/SpinField.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinField : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.eulerAngles += new Vector3(0.0f, 1.0f, 0.0f);
+    }
+}
