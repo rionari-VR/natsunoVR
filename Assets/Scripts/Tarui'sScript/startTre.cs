4a2fdad36b086ca342bb1737d2ab1a2dc7da7a35
TaruK981221 <tarui2412git@gmail.com>
2020-10-05 22:05:30 +0900

    ゲーム途中でフリーズするバグを修正 足元に出現する景品がおかしいバグを修正 その他修正

---
+++ Assets/Scripts/Tarui'sScript/startTre.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startTre : MonoBehaviour
+{
+    ItemOpe item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (this.transform.parent.TryGetComponent<ItemOpe>(out item))
+        {
+            this.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
