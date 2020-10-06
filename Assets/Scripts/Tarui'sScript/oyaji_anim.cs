f713a5cadd086790d4a287bf390358e76fd31de1
TaruK981221 <tarui2412git@gmail.com>
2020-10-06 13:01:59 +0900

    音楽用アセット追加 親父に音声を追加 BGM用プレハブ追加

--- Assets/Scripts/Tarui'sScript/oyaji_anim.cs
+++ Assets/Scripts/Tarui'sScript/oyaji_anim.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KanKikuchi.AudioManager;
 
 public class oyaji_anim : MonoBehaviour
 {
     Animator animator;
 
+    [SerializeField]
+    AudioClip hit1, hit2;
+
+    [SerializeField]
+    AudioClip start, end;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = this.GetComponent<Animator>();
+
+        SEManager.Instance.Play(start, 1, 0, 0.85f);
     }
 
     // Update is called once per frame
@@ -26,6 +35,15 @@ public class oyaji_anim : MonoBehaviour
         if (collision.gameObject.tag == "bullet")
         {
             animator.SetBool("oyaji", true);
+
+            if (Random.Range(0, 1) == 0)
+            {
+                SEManager.Instance.Play(hit1, 1, 0, 0.85f);
+            }
+            else
+            {
+                SEManager.Instance.Play(hit2, 1, 0, 0.85f);
+            }
         }
     }
 }
