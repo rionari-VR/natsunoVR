5a3bcc2293a2822eb2afb3dea3ffa8f8a36e030f
TaruK981221 <tarui2412git@gmail.com>
2020-10-06 11:58:17 +0900

    タグ追加 oyajiプレハブ追加 親父にアニメーション追加 bulletプレハブのタグを変更 その他修正

---
+++ Assets/Scripts/Tarui'sScript/oyaji_anim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyaji_anim : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (animator.GetBool("oyaji") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            animator.SetBool("oyaji", false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            animator.SetBool("oyaji", true);
+        }
+    }
+}
