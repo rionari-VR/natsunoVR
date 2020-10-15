3324ef8a8f3f59eb87e63f78b46cad77cf296854
ktsuji <aoriochiki@gmail.com>
2020-10-15 15:15:46 +0900

    プレイヤーprefabを追加しました。それに伴い、プレイヤーを操作するスクリプトも追加しました

---
+++ Assets/Scripts/Tsuji'sScript/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerController : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Vector2 trackPad;
+
+    Vector2 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trackPad = SteamVR_Actions.default_TrackPad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = trackPad.GetLastAxis(handType);
+        transform.localPosition = new Vector3(pos.x, 0.0f, pos.y);
+    }
+}
