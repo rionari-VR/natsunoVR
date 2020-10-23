3c58e29b7194b1c2ecc8c6bcf70307bcdeca2d0f
ktsuji <aoriochiki@gmail.com>
2020-10-23 09:51:33 +0900

    Update::player move

--- Assets/Scripts/Tsuji'sScript/PlayerController.cs
+++ Assets/Scripts/Tsuji'sScript/PlayerController.cs
@@ -7,19 +7,36 @@ public class PlayerController : MonoBehaviour
 {
     public SteamVR_Input_Sources handType;
     public SteamVR_Action_Vector2 trackPad;
+    public SteamVR_Input input;
+    public Camera camera;
 
-    Vector2 pos;
+    [SerializeField]
+    private int moveSpeedLimits;
+    private Vector2 pos;
 
     // Start is called before the first frame update
     void Start()
     {
         trackPad = SteamVR_Actions.default_TrackPad;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         pos = trackPad.GetLastAxis(handType);
-        transform.localPosition = new Vector3(pos.x, 0.0f, pos.y);
+        if(pos.y > 0)
+        {
+            //プレイヤーの向いている方向に動く
+            var temp = camera.transform.forward;
+            temp.y = 0.0f;
+            transform.localPosition += temp / moveSpeedLimits;
+        }
+        else if(pos.y < 0){
+            //プレイヤーのむいている反対側に動く
+            var temp = camera.transform.forward;
+            temp.y = 0.0f;
+            transform.localPosition -= temp / moveSpeedLimits;
+        }
     }
 }
