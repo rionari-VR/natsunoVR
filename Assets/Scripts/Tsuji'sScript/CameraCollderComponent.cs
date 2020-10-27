e2ecb612bb1de687f4637cae2a85487e519e02c6
ktsuji <aoriochiki@gmail.com>
2020-10-27 18:44:15 +0900

    Add::カメラの当たり判定を管理するスクリプトを追加しましま

---
+++ Assets/Scripts/Tsuji'sScript/CameraCollderComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollderComponent : MonoBehaviour
+{
+    //食べ物にカメラが当たったかどうか
+     private bool isEatFlag;
+
+    private void Start()
+    {
+        isEatFlag = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Food")
+        {
+            isEatFlag = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Food")
+        {
+            isEatFlag = true;
+        }
+    }
+
+    //Getter
+    public bool GetEatFlag()
+    {
+        return isEatFlag;
+    }
+}
