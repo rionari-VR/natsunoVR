6edb60ccdc175729f122d8a9905bc78b41ad4b15
TaruK981221 <tarui2412git@gmail.com>
2020-11-16 22:31:24 +0900

    水のシェーダー系を一気に追加

---
+++ Assets/Scripts/Tarui'sScript/WaterUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterUp : MonoBehaviour
+{
+    [SerializeField,Header("水の浮力")]
+    Vector3 UpPower = Vector3.zero;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>())
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+
+            rb.velocity += UpPower;
+            
+        }
+    }
+}
