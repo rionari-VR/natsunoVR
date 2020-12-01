f482d67923b96fd98fe7109fbf19301368f6a987
ktsuji <aoriochiki@gmail.com>
2020-12-01 18:12:21 +0900

    Add::歩くSEを追加

--- Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -5,6 +5,7 @@
 /// </summary>
 public static class SEPath{
 
+	public const string ASIOTO01           = "SE/FootSteps/asioto-01";
 	public const string BEAMGUN1           = "SE/Gun/beamgun1";
 	public const string BEAMGUNCHARGE1     = "SE/Gun/beam-gun-charge1";
 	public const string DRUMJAPANESE1      = "SE/Gun/drum-japanese1";
@@ -13,6 +14,9 @@ public static class SEPath{
 	public const string SNIPERRIFLEFIRING1 = "SE/Gun/sniperrifle--firing1";
 	public const string SYSTEM20           = "SE/system20";
 	public const string SYSTEM23           = "SE/system23";
+	public const string WALKFALLENLEAVES1  = "SE/FootSteps/walk-fallen-leaves1";
+	public const string WALKGRAVEL1        = "SE/FootSteps/walk-gravel1";
+	public const string WALKSOIL1          = "SE/FootSteps/walk-soil1";
 
 }
 
