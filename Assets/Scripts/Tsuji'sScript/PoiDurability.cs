a85b6464b12ecd89ed4a623e00bbe15c97d2dbbd
ktsuji <aoriochiki@gmail.com>
2020-12-01 18:16:23 +0900

    Add::Poiの耐久度処理を追加しました。

---
+++ Assets/Scripts/Tsuji'sScript/PoiDurability.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoiDurability : MonoBehaviour
+{
+    [SerializeField] int durability;  //耐久度
+
+    GameObject paper;
+    PoiPaperColliderComponent pPCC;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        paper = transform.GetChild(0).gameObject;
+        pPCC = GetComponentInChildren<PoiPaperColliderComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //当たってたらHP減少
+        if (pPCC.GetIsHitFlag())
+        {
+            durability--;
+            pPCC.SetIsHitFlag(false);
+        }
+
+        //耐久度0になったら紙が消える
+        if(durability <= 0)
+        {
+            Destroy(paper);
+        }
+    }
+}
