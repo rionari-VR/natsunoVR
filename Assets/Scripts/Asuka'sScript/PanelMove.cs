f9d3b26bf044196b4bd17bf59aa1b6991b613f77
Asuka1905 <asu.aka.051612>
2020-12-28 03:56:29 +0900

    プレイヤーのコリジョンのタグをPlayerにしたらパネルが出現するはず

---
+++ Assets/Scripts/Asuka'sScript/PanelMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelMove : MonoBehaviour
+{
+    public BoxCollider PanelCol;
+    Animator ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player"||
+            other.name==PanelCol.name)
+        {
+            ani.SetBool("HitPlayer", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+            ani.SetBool("HitPlayer", false);
+    }
+}
