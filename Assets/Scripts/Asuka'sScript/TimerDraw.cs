07b9f2104225d65567ef54edcc5512e4660b6a83
Asuka1905 <asu.aka.051612>
2021-02-02 17:39:08 +0900

    カウントダウンのタイマー作成した プレハブにあります。名前はCountDownTimerって名前のプレハブ

---
+++ Assets/Scripts/Asuka'sScript/TimerDraw.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerDraw : MonoBehaviour
+{
+    private int minute;
+    private float seconds;
+    private float oldSecond;
+    private Text TimerText;
+    [SerializeField, Header("制限時間(秒)")]
+    public int Limitsecond;
+
+    [SerializeField, Header("trueにすると制限時間リセット")]
+    public bool TimerReset;
+    [SerializeField, Header("制限時間が経過するとtrue")]
+    public bool TimerEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        minute = Limitsecond / 60;
+        seconds = Limitsecond % 60;
+        oldSecond = 0.0f;
+        TimerText = GetComponent<Text>();
+        TimerEnd = false;
+        TimerReset = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (minute >= 0 && seconds > 0)
+        {
+            seconds -= Time.deltaTime;
+        }else
+        {
+            TimerEnd = true;
+        }
+
+        if (seconds < 0 && minute > 0) 
+        {
+            minute--;
+            seconds = seconds + 60;
+        }
+
+        if ((int)seconds != (int)oldSecond)
+        {
+            TimerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+        }
+        oldSecond = seconds;
+
+        if (TimerReset)
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        minute = Limitsecond / 60;
+        seconds = Limitsecond % 60;
+        oldSecond = 0.0f;
+        TimerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+        TimerReset = false;
+        TimerEnd = false;
+    }
+}
